7d1bc8a380aec5856d5af5ba86f4c219922d6a27
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-07 16:51:48 +0000

    Deploy VokkeRr/tele2 to github.com/VokkeRr/tele2.git:gh-pages

---
+++ precache-manifest.b4a4a040346a695736efa0361686282b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac88553bea8565992a310672d4b95c1",
+    "url": "/tele2/index.html"
+  },
+  {
+    "revision": "655b92597499b0eeb723",
+    "url": "/tele2/static/css/main.85c0f7e9.chunk.css"
+  },
+  {
+    "revision": "e5139657e01045480748",
+    "url": "/tele2/static/js/2.0bfd1891.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tele2/static/js/2.0bfd1891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655b92597499b0eeb723",
+    "url": "/tele2/static/js/main.c3fd0add.chunk.js"
+  },
+  {
+    "revision": "1289a868035832ca9be3",
+    "url": "/tele2/static/js/runtime-main.435df064.js"
+  }
+]);
